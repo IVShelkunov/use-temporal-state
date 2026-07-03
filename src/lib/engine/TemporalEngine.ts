5f361f976ac031db43e38d899096fed086a28a78
Ilya Shelkunov <shelkunow48@gmail.com>
2026-07-03 15:38:43 +0300

    feat(core): implement vanilla temporal state engine with structuredClone support

---
+++ src/lib/engine/TemporalEngine.ts
@@ -0,0 +1,62 @@
+export class TemporalEngine<T> {
+    private past: T[] = [];
+    private present: T;
+    private future: T[] = [];
+    private clone: boolean;
+    constructor(initialValue: T, clone: boolean = false) {
+        this.clone = clone;
+        this.present = this.cloneValue(initialValue);
+    }
+    private cloneValue(value: T): T {
+        return this.clone ? structuredClone(value) : value;
+    }
+    public getPresent(): T {
+        return this.present;
+    }
+    public getPast(): T[] {
+        return this.past;
+    }
+    public getFuture(): T[] {
+        return this.future;
+    }
+    public canUndo(): boolean {
+        return this.past.length !== 0;
+
+    }
+    public canRedo(): boolean {
+        return this.future.length !== 0;
+    }
+    public set(newValue: T): void {
+        this.past.push(this.cloneValue(this.present));
+        this.present = this.cloneValue(newValue);
+        this.future = [];
+    }
+    public undo(): boolean {
+        if (this.canUndo()) {
+            this.future.push(this.cloneValue(this.present));
+            const previousState = this.past.pop();
+            if (previousState !== undefined) {
+                this.present = this.cloneValue(previousState);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public redo(): boolean {
+        if (this.canRedo()) {
+            const lastState = this.future.pop();
+            if (lastState !== undefined) {
+                this.past.push(this.cloneValue(this.present));
+                this.present = this.cloneValue(lastState);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public clear(): void {
+        this.past = [];
+        this.future = [];
+    }
+}
